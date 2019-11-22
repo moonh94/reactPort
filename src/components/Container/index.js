30dbf9c7b33e7a6eb4b2c3f176cbafc93b470a19
Hanmi Moon <hmmoon@hotmail.com>
2019-11-21 17:28:06 -0700

    trying to fix routing issue

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -12,9 +12,9 @@ class Container extends React.Component {
             <h2>Front End Web Developer</h2>
             <h3>
                 <ul className="list">
-                    <li><Link className="link" to="/about">About Me</Link></li>
-                    <li><Link className="link" to="/contact">Contact</Link></li>
-                    <li><Link className="link" to="/projects">Projects</Link></li>
+                    <li><Link className="link" to="#about">About Me</Link></li>
+                    <li><Link className="link" to="#contact">Contact</Link></li>
+                    <li><Link className="link" to="#projects">Projects</Link></li>
                 </ul>
             </h3>
             <h1>I'm Han Mi Moon</h1>
